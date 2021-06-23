923813a0601880d978fa3428e9ec15fcad359d4d
nchannelmosfet <nchannelmosfet@gmail.com>
2021-06-22 21:08:46 -0700

    Update test_data_crawler.py

--- test/test_data_crawler.py
+++ test/test_data_crawler.py
@@ -2,15 +2,16 @@ from dkcrawlerv2 import AsyncDataCrawler, AsyncDataCrawlerRunner
 import asyncio
 
 
-async def main():
-    # start_urls = 'https://www.digikey.com/en/products/filter/thermal-heat-pipes-vapor-chambers/977'
-    # base_download_dir = r'../download'
-    # crawler = AsyncDataCrawler(start_url, base_download_dir, headless=True)
-    # await crawler.crawl()
+async def test_one_data_crawler():
+    start_url = 'https://www.digikey.com/en/products/filter/thermal-heat-pipes-vapor-chambers/977'
+    base_download_dir = r'../download'
+    crawler = AsyncDataCrawler(start_url, base_download_dir, headless=True)
+    await crawler.crawl()
 
+
+async def test_data_crawler_runner():
     start_urls = [
         'https://www.digikey.com/en/products/filter/thermal-thermoelectric-peltier-modules/222',
-        'https://www.digikey.com/en/products/filter/thermal-heat-pipes-vapor-chambers/977',
         'https://www.digikey.com/en/products/filter/thermal-flexible-heaters/1005',
         'https://www.digikey.com/en/products/filter/fans-finger-guards-filters-sleeves/221',
         'https://www.digikey.com/en/products/filter/fans-accessories/223',
@@ -19,6 +20,11 @@ async def main():
     base_download_dir = r'../download'
     crawler_runner = AsyncDataCrawlerRunner(start_urls, base_download_dir, headless=False)
     await crawler_runner.crawl()
+
+
+async def main():
+    await test_one_data_crawler()
+    await test_data_crawler_runner()
 
 
 if __name__ == '__main__':
