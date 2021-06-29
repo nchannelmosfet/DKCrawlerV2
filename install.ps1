a0a412788a370486d6f7b7a76aa1cfc566fd5c61
nchannelmosfet <nchannelmosfet@gmail.com>
2021-06-28 20:47:55 -0700

    updated requirements, added update.ps1

--- install.ps1
+++ install.ps1
@@ -3,7 +3,3 @@ py -3.9-64 -m virtualenv venv
 .\venv\Scripts\activate
 pip install -r requirements.txt
 playwright install
-
-jupyter nbextension enable --py widgetsnbextension
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
-jupyter lab build
