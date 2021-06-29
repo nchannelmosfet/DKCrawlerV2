4ab0d3059d159ede6f082d43c9cfd5e15abfc32b
JerryWu <jwu@digitalairstrike.com>
2021-06-29 15:56:55 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,7 +4,7 @@ py -3-64 -m virtualenv venv
 pip install -r requirements.txt
 playwright install
 
-$items_to_hide = ".\.gitignore", ".\.git", "requirements.txt", ".\venv", ".idea", "dkcrawlerv2", 'test', 'setup.py'
+$items_to_hide = ".gitignore", ".git", "requirements.txt", "venv", ".idea", "dkcrawlerv2", 'test', 'setup.py'
 foreach ($path in $items_to_hide) {
 	if (Test-Path -path $path){
 		$item = Get-Item $path -Force
