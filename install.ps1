783262e9e829c93e8539682b4b88173d1f6d5307
nchannelmosfet <nchannelmosfet@gmail.com>
2021-07-01 18:18:30 -0700

    added instructions

--- install.ps1
+++ install.ps1
@@ -1,8 +1,8 @@
-py -3-64 -m pip install virtualenv
-py -3-64 -m virtualenv venv
-.\venv\Scripts\activate
-pip install -r requirements.txt
-playwright install
+#py -3-64 -m pip install virtualenv
+#py -3-64 -m virtualenv venv
+#.\venv\Scripts\activate
+#pip install -r requirements.txt
+#playwright install
 
 $items_to_hide = ".gitignore", ".git", "requirements.txt", "venv", ".idea", "dkcrawlerv2", 'test', 'setup.py'
 foreach ($path in $items_to_hide) {
