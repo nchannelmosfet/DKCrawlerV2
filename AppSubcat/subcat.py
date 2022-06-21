a5496fbe45dd6be979318cbd9a7afb720a69bfec
nchannelmosfet <nchannelmosfet@gmail.com>
2022-06-20 19:27:32 -0700

    Update subcat.py

--- AppSubcat/subcat.py
+++ AppSubcat/subcat.py
@@ -9,6 +9,9 @@ async def main():
     start_urls = start_urls or read_urls('subcat_urls.txt')
     base_download_dir = os.path.join(os.path.dirname(__file__), 'downloads')
 
+    if not os.path.exists(base_download_dir):
+        os.mkdir(base_download_dir)
+
     headless = True
     session_name = None
     crawler_runner = AsyncDataCrawlerRunner(
